86518ffca632480e92fe9eea7f58071869aff623
rblakedavis <r.blake.davis@gmail.com>
2024-01-08 20:31:15 -0500

    push for nightly update 1-8-24

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public static class Data 
 {
     public static string curZoneName;
-    public static string[] zoneNames;
-    public static int levelCur;
-    public static int magicMax;
-    public static int magicCur;
-    public static int healthMax;
-    public static int healthCur;
+    public static string[] zoneNames = {"Forest", "Cave", "Ruins", "Depths", "Underworld"};
+    public static int levelCur = 1;
+    public static int magicMax = 2;
+    public static int magicCur = 0;
+    public static int healthMax = 15;
+    public static int healthCur = 15;
     public static string subWindowText = "example text";
     public static int goldCur;
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,16 +13,67 @@ public class GameManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI subWindow;
     [SerializeField] private SceneChanger sceneChanger;
 
-    void OnEnable()
+    private static GameManager _instance;
+    private bool isInitialized = false;
+
+    public static GameManager Instance
     {
-        Debug.Log("On Enable");
-        Data.subWindowText = string.Empty;
-        subWindow.text = Data.subWindowText;
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+                if (_instance == null)
+                {
+                    GameObject singleton = new GameObject(typeof(GameManager).Name);
+                    _instance = singleton.AddComponent<GameManager>();
+                }
+            }
+            return _instance;
+        }
     }
+
+    private void Awake()
+    {
+        if(_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        if (!isInitialized)
+        {
+
+            Debug.Log("Start");
+            Data.subWindowText = "Welcome to the game.";
+            subWindow.text = Data.subWindowText;
+            zone.text=Data.zoneNames[0];
+            level.text = Data.levelCur.ToString();
+            magic.text = Data.magicCur.ToString();
+            health.text = Data.healthCur.ToString();
+            
+        }
+    }
+
+    private void OnSceneLoaded()
+    {
+        //update any pertinent variables here
+    } 
 
     // Update is called once per frame
     void Update()
     {
+
+        Camera.main.aspect = 4f / 3f;
+
+
         if (subWindow.text != Data.subWindowText)
             {
                 subWindow.text = Data.subWindowText;
